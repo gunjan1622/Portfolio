b00436e3be5f3c295ab9bb3f6a51bf665167737c
Gunjan Agrawal <86597566+gunjan1622@users.noreply.github.com>
2023-09-23 20:20:55 +0530

    Form

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -135,10 +135,11 @@ let footer = $(`
              <div class="form-header">
                 <h6 class="display">Get in Touch</h6>
               </div>
-                <form name="form1" action="https://formcarry.com/s/dnqp3iFwXN" method="POST" accept-charset="UTF-8" >
+                <form action="https://formcarry.com/s/SvZW504I7a" method="POST" accept-charset="UTF-8">
                   <input id="name" type="text" name="name" placeholder="Your Name" required/>
                   <input id="email" type="email" name="email" placeholder="Email Address" required/>                  
                   <textarea id="textArea" name="message" placeholder="Type your Message" required></textarea>
+                  <input type="hidden" name="_gotcha">
               
                   <div id="main">
                     <button id="lnch" type="button" value="Send" >Send</button>
