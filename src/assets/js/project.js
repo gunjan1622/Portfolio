c72a74a18b9906d82b4f2e1bcc6fd507f68aad80
Gunjan Agrawal <86597566+gunjan1622@users.noreply.github.com>
2023-09-23 12:38:41 +0530

    portfolio update

--- src/assets/js/project.js
+++ src/assets/js/project.js
@@ -9,38 +9,38 @@ const projects = [
     title: "Intelligent Question Generator",
     cardImage: "/src/assets/images/project-page/qa.png",
     Previewlink:
-      "https://huggingface.co/spaces/HemanthSai7/IntelligentQuestionGenerator",
-    Githublink: "https://github.com/HemanthSai7/Intelligent-Question-Generator",
+      "https://huggingface.co/spaces/Gunjan1622/IntelligentQuestionGenerator",
+    Githublink: "https://github.com/Gunjan1622/Intelligent-Question-Generator",
   },
   {
     title: "Solar Panel dust detection",
     cardImage: "/src/assets/images/project-page/solar.jpeg",
     Previewlink: "",
-    Githublink: "https://github.com/HemanthSai7/uae-chapter-dust-solar-panels",
+    Githublink: "https://github.com/Gunjan1622/uae-chapter-dust-solar-panels",
   },
   {
     title: "Sign Language Recognition",
     cardImage: "/src/assets/images/project-page/sl.png",
     Previewlink: "https://signlanguage.streamlitapp.com/",
-    Githublink: "https://github.com/HemanthSai7/SignLanguage-Pytorch",
+    Githublink: "https://github.com/Gunjan1622/SignLanguage-Pytorch",
   },
   {
     title: "Termsbrief",
     cardImage: "/src/assets/images/project-page/termsbrief.jpg",
     Previewlink: "https://tncsummariser.streamlit.app/",
-    Githublink: "https://github.com/HemanthSai7/TnC",
+    Githublink: "https://github.com/Gunjan1622/TnC",
   },
   {
     title: "EligiLoan",
     cardImage: "/src/assets/images/project-page/eligiloan.jpg",
     Previewlink: "https://eligiloan.streamlit.app/",
-    Githublink: "https://github.com/HemanthSai7/EligiLoan",
+    Githublink: "https://github.com/Gunjan1622/EligiLoan",
   },
   {
     title: "CapTraduire",
     cardImage: "/src/assets/images/project-page/captraduire.jpg",
     Previewlink: "",
-    Githublink: "https://github.com/HemanthSai7/CapTraduire",
+    Githublink: "https://github.com/Gunjan1622/CapTraduire",
   },
   {
     title: "Glow Green",
@@ -52,7 +52,7 @@ const projects = [
     title: "Cassava Leaf Disease Classification",
     cardImage: "/src/assets/images/project-page/cld2.png",
     Previewlink: "https://cassava-gdsc.streamlit.app/",
-    Githublink: "https://github.com/HemanthSai7/Arch-DL",
+    Githublink: "https://github.com/Gunjan1622/Arch-DL",
   },
   {
     title: "Music and Song Recommendation Engine",
@@ -64,7 +64,7 @@ const projects = [
     title: "Scrapster",
     cardImage: "/src/assets/images/project-page/scrap.png",
     Previewlink: "",
-    Githublink: "https://github.com/HemanthSai7/Scrapster",
+    Githublink: "https://github.com/Gunjan1622/Scrapster",
   },
 ];
 
